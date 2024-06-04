c6baa3c4ed9bfa6a4df288f4e8885b6ea970a607
David Conner <robotics@cnu.edu>
2024-06-04 00:01:06 -0400

    flake8/pycodestyle cleanup

--- flexbe_states/setup.py
+++ flexbe_states/setup.py
@@ -2,8 +2,8 @@
 
 from glob import glob
 
-from setuptools import setup
 from setuptools import find_packages
+from setuptools import setup
 
 PACKAGE_NAME = 'flexbe_states'
 
@@ -14,9 +14,9 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + PACKAGE_NAME]),
         ('share/' + PACKAGE_NAME, ['package.xml']),
-        ('share/' + PACKAGE_NAME + "/tests", glob('tests/*.test')),
-        ('share/' + PACKAGE_NAME + "/launch", glob('tests/*.launch.py')),
-        # ros2 bag issues - ('share/' + PACKAGE_NAME + "/tests/bags", glob('tests/bags/*.bag')),
+        ('share/' + PACKAGE_NAME + '/tests', glob('tests/*.test')),
+        ('share/' + PACKAGE_NAME + '/launch', glob('tests/*.launch.py')),
+        # ros2 bag issues - ('share/' + PACKAGE_NAME + '/tests/bags', glob('tests/bags/*.bag')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
